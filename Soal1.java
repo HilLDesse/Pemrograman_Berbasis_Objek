5f47fd6af57a93f9390aa06c69dea92c5d001627
HilLDesse <hilmikaut@gmail.com>
2026-09-11 23:39:32 +0700

    Update : Pengerjaan Soal 2 dan 3

--- Soal1.java
+++ Soal1.java
@@ -26,7 +26,6 @@ public class Soal1 {
                 System.out.println(sc.next() + " can't be fitted anywhere.");
             }
         }
-        
         sc.close();
     }
 }
---
+++ Soal3.java
@@ -0,0 +1,7 @@
+class Soal3 {
+    public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        System.out.println("Nilai nx : " + nx);
+    }
+}
